882ddaaed5d060b845b5efff261a170473298de3
RicardMontfort90 <ricard.montfort.romero@gmail.com>
2023-02-23 15:01:43 +0100

    work in progress

--- my-app/src/components/Card.jsx
+++ my-app/src/components/Card.jsx
@@ -5,11 +5,29 @@ import icon4 from '../images/icon4.png';
 
 const Card = () => {
     return (
-        <div>
-            
+        <div className='card-container'>
+            <div className='card'>
+                <img src={icon1} alt='icon' />
+                <h2>Declarative</h2>
+                <p>React makes it painless to create interactive UIs.</p>
+            </div>
+            <div className='card'>
+                <img src={icon2} alt='icon' />
+                <h2>Components</h2>
+                <p>Build encapsulated components that manage their state.</p>
+            </div>
+            <div className="card">
+                <img src={icon3} alt="icon" />
+                <h2>Single-Way</h2>
+                <p>A set of immutable values are passed to the component's</p>
+            </div>
+            <div className="card">
+                <img src={icon4} alt="icon" />
+                <h2>JSX</h2>
+                <p>Statically-typed, design to run in modern browsers.</p>
+            </div>   
         </div>
-    )
+    );
 }
-
 
 export default Card;
