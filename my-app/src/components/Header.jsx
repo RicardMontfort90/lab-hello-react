4ff27725598a799a6606e5c5432f48530da22921
RicardMontfort90 <ricard.montfort.romero@gmail.com>
2023-02-23 15:05:28 +0100

    work in progress

--- my-app/src/components/Header.jsx
+++ my-app/src/components/Header.jsx
@@ -3,7 +3,14 @@ import Navbar from "./Navbar";
 const Header = () => {
     return (
         <div className="header">
-        
+            <Navbar />
+            <div className="header-container">
+                <h1>Say Hello to ReactJS</h1>
+                <p>You will learn how to use the most popular front-end library, and become a super Ninja developer.</p>
+                <button>Awsome!</button>
+            </div>
         </div>
-    )
+    );
 }
+
+export default Header;
