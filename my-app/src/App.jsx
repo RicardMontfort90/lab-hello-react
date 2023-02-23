6acce82480164e7805a47a9b15de4de02e0269e1
RicardMontfort90 <ricard.montfort.romero@gmail.com>
2023-02-23 14:47:43 +0100

    work in progress

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -26,10 +26,15 @@ export default App; */
 
 // src/App.js
 import './App.css';
+import Header from './components/Header';
+import Card from './components/Card';
 
 function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+      <Header />
+      <Card />
+    </div>
   );
 }
 export default App;
---
+++ my-app/src/components/Card.jsx
@@ -0,0 +1,15 @@
+import icon1 from '../images/icon1.png';
+import icon2 from '../images/icon2.png';
+import icon3 from '../images/icon3.png';
+import icon4 from '../images/icon4.png';
+
+const Card = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+
+export default Card;
